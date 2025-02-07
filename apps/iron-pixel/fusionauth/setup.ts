05fdbb74a86f5179b456e77c7dcf3e5b790773ff
Alex Patterson <alex@codingcat.dev>
2025-02-07 00:57:59 -0500

    tenant, key, admin

--- apps/iron-pixel/fusionauth/setup.ts
+++ apps/iron-pixel/fusionauth/setup.ts
@@ -4,11 +4,18 @@ import chalk from 'chalk';
 interface ApiConfig {
   key: string;
   endpoint: string;
+  adminEmail: string;
+  adminPassword: string;
+  adminUserId?: string;
+  name?: string;
   tenantId?: string;
+  signingKeyId?: string;
 }
 
 class ApiRunner {
   constructor(private config: ApiConfig) {
+    this.config.name = 'iron-pixel';
+
     if (!this.config.key) {
       this.config.key =
         'this_really_should_be_a_long_random_alphanumeric_value_but_this_still_works';
@@ -17,44 +24,152 @@ class ApiRunner {
     if (!this.config.endpoint) {
       this.config.endpoint = 'http://localhost:9011';
     }
+
+    if (!this.config.adminEmail) {
+      this.config.adminEmail = 'admin@example.com';
+    }
+
+    if (!this.config.adminPassword) {
+      this.config.adminPassword = 'password';
+    }
+
     this.config.tenantId = '';
   }
 
   private async tenantCreate() {
-    const name = 'iron-pixel';
-    const resp = await this.runSingleApi('/api/tenant', {
+    console.log(chalk.blue('Creating tenant'));
+    const resp = await this.singleApi('/api/tenant', {
       method: 'POST',
       body: JSON.stringify({
         tenant: {
-          name,
+          name: `${this.config.name}-tenant`,
         },
       }),
     });
     if (!resp?.data?.tenant?.id) {
       console.log(
-        chalk.red('Failed to create tenant:', JSON.stringify(resp.data))
+        chalk.yellow('Failed to create tenant:', JSON.stringify(resp.data))
       );
+      console.log(chalk.blue('Checking if tenant exists'));
+      const tenantExistsResp = await this.singleApi('/api/tenant/search', {
+        method: 'POST',
+        body: JSON.stringify({
+          search: {
+            name: `${this.config.name}-tenant`,
+            numberOfResults: 1,
+            orderBy: 'name',
+            startRow: 0,
+          },
+        }),
+      });
+      if (!tenantExistsResp.ok) {
+        console.log(chalk.red('Tenant does not exist'));
+        return;
+      }
+      console.log(
+        chalk.green(
+          'Tenant exists, id:',
+          JSON.stringify(tenantExistsResp.data.tenants[0].id)
+        )
+      );
+      this.config.tenantId = tenantExistsResp?.data?.tenants[0]?.id;
       return;
     }
     console.log(chalk.green('Tenant created:', resp?.data?.tenant?.id));
     this.config.tenantId = resp?.data?.tenant?.id;
   }
 
-  // Create Key to match Auth.js HMAC 256
-  // await this.runSingleApi('/api/key/generate/#{asymmetricKeyId}', {
-  //   method: 'POST',
-  //   body: JSON.stringify({
-  //     key: {
-  //       algorithm: 'RS256',
-  //       name: 'For exampleapp',
-  //       length: 2048,
-  //     },
-  //   }),
-  // });
+  private async createKey() {
+    console.log(chalk.blue('Creating key'));
+    const resp = await this.singleApi('/api/key/generate', {
+      method: 'POST',
+      body: JSON.stringify({
+        key: {
+          algorithm: 'RS256',
+          name: `${this.config.name}-signing-key`,
+          length: 2048,
+        },
+      }),
+    });
+    if (!resp?.data?.key?.id) {
+      console.log(
+        chalk.yellow('Failed to create key:', JSON.stringify(resp.data))
+      );
+      console.log(chalk.blue('Checking if key exists'));
+      const search = await this.singleApi('/api/key/search', {
+        method: 'POST',
+        body: JSON.stringify({
+          search: {
+            name: `${this.config.name}-signing-key`,
+            numberOfResults: 1,
+            orderBy: 'name',
+            startRow: 0,
+          },
+        }),
+      });
+      if (!search.ok) {
+        console.log(chalk.red('Key does not exist'));
+        return;
+      }
+      console.log(
+        chalk.green('Key exists, id:', JSON.stringify(search.data.keys[0].id))
+      );
+      this.config.signingKeyId = search?.data?.keys[0]?.id;
+      return;
+    }
+    console.log(chalk.green('Key created:', resp?.data?.key?.id));
+    this.config.signingKeyId = resp?.data?.key?.id;
+  }
 
-  private async runSingleApi(
+  private async createAdminUser() {
+    console.log(chalk.blue('Creating admin user'));
+    const resp = await this.singleApi('/api/user/registration', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: {
+          email: this.config.adminEmail,
+          password: this.config.adminPassword,
+        },
+        registration: {
+          applicationId: '3c219e58-ed0e-4b18-ad48-f4f92793ae32', // Hate this but it's what the docs say
+          roles: ['admin'],
+        },
+      }),
+    });
+    if (!resp?.data?.key?.id) {
+      console.log(
+        chalk.yellow('Failed to create admin user:', JSON.stringify(resp.data))
+      );
+      console.log(chalk.blue('Checking if admin user exists'));
+      const search = await this.singleApi('/api/user/search', {
+        method: 'POST',
+        body: JSON.stringify({
+          search: {
+            queryString: this.config.adminEmail,
+          },
+        }),
+      });
+      if (!search.ok) {
+        console.log(chalk.red('Admin user does not exist'));
+        return;
+      }
+      console.log(
+        chalk.green(
+          'Admin user exists, id:',
+          JSON.stringify(search.data.users[0].id)
+        )
+      );
+      this.config.adminUserId = search?.data?.users[0]?.id;
+      return;
+    }
+    console.log(chalk.green('Admin user created:', resp?.data?.key?.id));
+    this.config.adminUserId = resp?.data?.key?.id;
+  }
+
+  private async singleApi(
     apiPath: string,
     options?: RequestInit
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   ): Promise<{ ok: boolean; data: any }> {
     try {
       console.log(chalk.blue(`Calling API: ${apiPath}`));
@@ -90,7 +205,9 @@ class ApiRunner {
 
   public async runAll() {
     console.log(chalk.green('Starting setup'));
-    const tenant = await this.tenantCreate();
+    await this.tenantCreate();
+    await this.createKey();
+    await this.createAdminUser();
   }
 }
 
@@ -105,13 +222,18 @@ async function main() {
 
   // Get API configuration
   const apiKey = await question(
-    'Enter API key(this_really_should_be_a_long_random_alphanumeric_value_but_this_still_works): '
+    'API key(this_really_should_be_a_long_random_alphanumeric_value_but_this_still_works): '
   );
-  const endpoint = await question(
-    'Enter API endpoint (http://localhost:9011): '
-  );
+  const endpoint = await question('API endpoint (http://localhost:9011): ');
+  const adminEmail = await question('Admin Email (admin@example.com): ');
+  const adminPassword = await question('Admin Password (password): ');
 
-  const runner = new ApiRunner({ key: apiKey, endpoint });
+  const runner = new ApiRunner({
+    key: apiKey,
+    endpoint,
+    adminEmail,
+    adminPassword,
+  });
 
   await runner.runAll();
 

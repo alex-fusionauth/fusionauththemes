fbdbd7897e1e2ae9e24033a9621642337fc4f4b9
Alex Patterson <alex@codingcat.dev>
2025-02-25 08:41:05 -0500

    initial fumadocs

--- apps/main/app/(home)/page.tsx
+++ apps/main/app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent } from '@/app/components/ui/card';
+import { Card, CardContent } from '@/app/components/card';
 import Image from 'next/image';
 import Link from 'next/link';
 

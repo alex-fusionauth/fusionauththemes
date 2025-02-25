39141bcd32f51065e8d6623d30b5df1bd326d332
Alex Patterson <alex@codingcat.dev>
2025-02-25 08:41:05 -0500

    initial fumadocs

---
+++ apps/main/lib/iron-pixel.tsx
@@ -0,0 +1,71 @@
+export default function IronPixel() {
+  return (
+    <svg
+      width="41"
+      height="33"
+      viewBox="0 0 41 33"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>Iron Pixel</title>
+      <path
+        d="M5.33483 6.80489L20.6458 13.6098V32.3232L0.571411 22.1159L5.33483 6.80489Z"
+        fill="url(#paint0_radial_316_1804)"
+      />
+      <path
+        d="M35.9568 6.80489L20.6458 13.6098V32.3232L40.7202 22.1159L35.9568 6.80489Z"
+        fill="url(#paint1_radial_316_1804)"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M20.6458 2.97868L33.2349 8.57381V14.9707H8.05679V8.57381L20.6458 2.97868ZM5.33483 6.80489L20.6458 0L35.9568 6.80489V17.6927H5.33483V6.80489Z"
+        fill="url(#paint2_radial_316_1804)"
+      />
+      <defs>
+        <radialGradient
+          id="paint0_radial_316_1804"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(37.8566 6.13697) rotate(156.176) scale(42.0862 42.0862)"
+        >
+          <stop stop-color="#1AB7FF" />
+          <stop offset="0.246584" stop-color="#0DC7E3" />
+          <stop offset="0.444146" stop-color="#08C4B6" />
+          <stop offset="0.662916" stop-color="#00C288" />
+          <stop offset="1" stop-color="#00C750" />
+        </radialGradient>
+        <radialGradient
+          id="paint1_radial_316_1804"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(37.8566 6.13697) rotate(156.176) scale(42.0862 42.0862)"
+        >
+          <stop stop-color="#1AB7FF" />
+          <stop offset="0.246584" stop-color="#0DC7E3" />
+          <stop offset="0.444146" stop-color="#08C4B6" />
+          <stop offset="0.662916" stop-color="#00C288" />
+          <stop offset="1" stop-color="#00C750" />
+        </radialGradient>
+        <radialGradient
+          id="paint2_radial_316_1804"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(37.8566 6.13697) rotate(156.176) scale(42.0862 42.0862)"
+        >
+          <stop stop-color="#1AB7FF" />
+          <stop offset="0.246584" stop-color="#0DC7E3" />
+          <stop offset="0.444146" stop-color="#08C4B6" />
+          <stop offset="0.662916" stop-color="#00C288" />
+          <stop offset="1" stop-color="#00C750" />
+        </radialGradient>
+      </defs>
+    </svg>
+  );
+}
--- apps/main/lib/source.ts
+++ apps/main/lib/source.ts
@@ -1,7 +1,24 @@
 import { docs } from '@/.source';
 import { loader } from 'fumadocs-core/source';
+import { icons } from 'lucide-react';
+import { createElement } from 'react';
+import ChangeBank from './changebank';
+import IronPixel from './iron-pixel';
 
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (!icon) {
+      // You may set a default icon
+      return;
+    }
+    if (icon === 'iron-pixel') {
+      return createElement(IronPixel);
+    }
+    if (icon === 'changebank') {
+      return createElement(ChangeBank);
+    }
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  },
 });

ce3b1d830eb28fec426625d67622664b67cd3181
Alex Patterson <alex@codingcat.dev>
2025-03-31 10:57:26 -0400

    add privacy policies

---
+++ apps/finexa/app/privacy-policy/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Privacy() {
+  return (
+    <section className="relative mt-80 text-primary-foreground flex flex-col items-center justify-center gap-4 p-4 text-center">
+      This is an example application, please see{' '}
+      <Link
+        className="underline"
+        target="_blank"
+        href="https://fusionauth.io/privacy-policy"
+      >
+        FusionAuth's Privacy Policy
+      </Link>{' '}
+      for more information.
+    </section>
+  );
+}
